33a4df43ac127ef61787309e16e80dc09467293a
rhrits <hritik.hritiksingh@gmail.com>
2024-09-06 05:51:43 +0530

    n

--- basic/Loop/patterns/fullpyramid.cpp
+++ basic/Loop/patterns/fullpyramid.cpp
@@ -3,6 +3,19 @@
 using namespace std;
 
 int main(){
+
+    int n;
+    cin >> n;
+
+    //rows - outer loop
+    for (int row = 0; row < n; row++){
+
+        // spaces 
+
+        for (int col = 0 ; col < n - (row + 1); col++){
+            
+        }
+    }
    
 
 }
