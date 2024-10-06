257c6d043a21ce34aabe00ff57508f9a6802697a
rhrits <hritik.hritiksingh@gmail.com>
2024-10-07 02:56:10 +0530

    mod

--- basic/Loop/patterns/NumericHollowHalfPyramid.cpp
+++ basic/Loop/patterns/NumericHollowHalfPyramid.cpp
@@ -3,6 +3,15 @@ using namespace std;
 int main(){
 
     for(int row = 0 ; row < 5; row++){
-        for(in col = 0; )
+        for(int col = 0; col <= row ; col++){
+            if(col == 0 || col == row || row == 4){
+                cout << col + 1;
+            }
+            else{
+                cout<< " ";
+            }
+      
+        }
+        cout << endl;
     }
 }
